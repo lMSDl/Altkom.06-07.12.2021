ad87558e59bcd19cf6f97a9e08e2965b3f596ff8
Paweł Łęczycki <pawel@msd.info.pl>
2021-12-07 12:43:46 +0100

    CsvDataProvider

---
+++ Services.InMemory/CsvDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CsvDataProvider<T> : FileDataProvider<T>
+    {
+        public CsvDataProvider(string path) : base(path)
+        {
+        }
+
+        protected override ICollection<T> DeserializeCache(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SerializeCache(IEnumerable<T> cache)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
